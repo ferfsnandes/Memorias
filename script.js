19768447d37aa155335cf72c564574a4aa0cd322
idelay <98506951+idelay@users.noreply.github.com>
2022-09-16 23:40:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ boardCreation()
 })
 
 
-// When the user clicks on <div>, open the popup
+// When the user clicks on "Reiniciar?", open the popup
 function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
